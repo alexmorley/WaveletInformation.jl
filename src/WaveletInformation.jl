846d6973433819a76adf7eed845b4893e4e654ee
Alexander Morley <alexander.morley@univ.ox.ac.uk>
2017-07-13 14:38:59 +0100

    Wavelet PCA

--- src/WaveletInformation.jl
+++ src/WaveletInformation.jl
@@ -4,8 +4,13 @@ using Reexport
 @reexport using MLBase
 using ScikitLearn
 using Wavelets
+using Combinatorics
+using MultivariateStats
+using GaussianMixtures
+using Suppressor
 
 include("wavelet_decode.jl")
 include("MI.jl")
+include("wpca.jl")
 
 end
---
+++ src/wpca.jl
@@ -0,0 +1,86 @@
+function wavedec2d{T<:AbstractFloat}(clips::Array{T,2},
+                   wavtype = WT.haar)
+    # level of wavelet transform
+    wt = wavelet(wavtype)
+    N = floor(Int,log2(size(clips,2)))
+    # intialise array
+    components_all = zeros(clips)
+    for i in 1:size(components_all,1)
+        components_all[i,:] = dwt(clips[i,:], wt, N)
+    end
+    return components_all::Array{T,2}
+end
+
+function rungmm{T}(component::Array{T,1}, ngauss::Int, covtype=:full, nIter=30, perr=false)    
+    if perr
+        mm = GMM(ngauss, Float64.(component[:,1:1]),
+            kind=covtype, nIter=nIter)
+    else
+    @capture_err begin; @capture_out begin
+        mm = GMM(ngauss, Float64.(component[:,1:1]),
+            kind=covtype, nIter=nIter)
+    end; end
+    end
+    return mm
+end
+    
+function rungmm{T<:AbstractFloat}(norm_components::Array{T,2},
+    ngauss::Int, covtype=:full, nIter=30, v=false, vv=false)
+    ncomps = size(norm_components,2)
+    mms = []
+    for ncomp in 1:ncomps
+        v && println("Fitting to $ncomp/$ncomps components")
+        push!(mms, rungmm(norm_components[:,ncomp],
+            ngauss, covtype, nIter, vv))
+    end
+    mms
+end
+    
+function getsep(mm, ngauss::Int)
+    sep = 0
+    for (i,j) in collect(combinations(1:ngauss,2))
+        sep += (abs(mm.μ[i] - mm.μ[j]) * sqrt(mm.w[i] * mm.w[j])) /
+            sqrt((mm.Σ[i][1]*mm.Σ[i][1]) * (mm.Σ[j][1]*mm.Σ[j][1]))
+    end
+    return sep
+end
+
+function check_seps(seps::Vector{<:Real},verbose::Bool=false)
+    sepnans = isnan.(seps)
+    if any(sepnans)
+        verbose & warn("Some NaNs in seperability scores, setting to zero")
+        seps[sepnans] = zero(eltype(seps))
+    end
+    seps
+end
+
+function weightedpca(clips)
+    #### options ####
+    ngauss = 4 # number of gaussians to fit to the wavelet components
+    v = true # verbosity
+    vv = false # double verbosity
+    sep_flag = true # calculate seperability?
+    nIterGMM = 500 # niterations for GMM
+
+    v && println("Starting weighted PCA...") 
+    # apply harr wavelet to each waveform
+    println("Applying Wavelets ...")
+    wavelet_components = wavedec2d(clips, WT.haar)
+    nof_components = size(wavelet_components,2)
+    norm_components = zscore(wavelet_components,1) # (data-mean)/std
+
+    if sep_flag
+        println("Running GMM ...")
+        # fit ngauss gaussians to each component
+        mms = rungmm(norm_components, ngauss, :diag, nIterGMM, v, vv)
+        # get seperability metric (i.e. how far from unimodal distribution)
+        seps = getsep.(mms, [ngauss])
+        seps = check_seps(seps,vv)
+    else
+        seps=ones(nof_components)
+    end
+    
+    wpca = fit(PCA,norm_components.*seps');
+
+    return wpca.proj
+end
